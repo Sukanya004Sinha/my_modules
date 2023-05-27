875e04b5070159f25ac7644f74d7ae1ecac4d534
sukanya004sinha <sukanyasinha004@gmail.com>
2023-05-27 17:52:23 +0530

    changes for hooks revision

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import RulesHooks from './component/rulesHooks';
+const App = () => {
+//   var val = 'sparm';
+//   //console.log(useState('sukku'));
+//  // let myfirstVal = useState('sukanyass')[0];
+//  // console.log(myfirstVal);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//  //const [initialState,func]  = useState('sukanya sinha')
+// const [myName,setMyName] = useState('sukanyasinha');
+//   const changeName = () => {
+//    // val = "sukanya";
+//     //console.log(myName);
+//     setMyName ('sparm');
+     
+// let val = myName;
+// /**** 1way
+//  *  if(val === 'sukanyasinha'){
+//   setMyName('sparm');
+// }
+// else{
+//   setMyName('sukanyasinha');
+// }
+//   **/  
+//  //2 way using ternary operator
+
+//  (val === 'sukanyasinha') ? setMyName ('sparm') : setMyName ('sukanyasinha');
+// }
+// console.log(myName);
+  // return (
+  //   <div>
+  //   <h1>{myName}</h1>
+  //   <button className="btn" onClick={changeName} >Click me please</button>
+  //   </div>
+  // )
+//}
+return (
+  <div>
+  <RulesHooks />
+  </div>
+)
 }
 
 export default App;
---
+++ src/component/rulesHooks.js
@@ -0,0 +1,23 @@
+/*******
+ * Always write it inside the component or function
+ * Component must be pascalCase (first letter should be uppercase)
+ * we can directly import or we can directly write it using hookName 
+ * Don't call hooks inside loops , condition, or in nested function
+ * 
+ */
+import React from 'react'
+//import React , {useState} from 'react'
+
+
+function RulesHooks() {
+   
+    const  [myName, setMyName] = useState('sukanya');
+    
+  return (
+    <div>
+    <h1>{myName}</h1>
+    </div>
+  )
+}
+
+export default RulesHooks
